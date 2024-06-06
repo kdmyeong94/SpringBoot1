c330a4dbd042b782667aaa7e402db96568f2dad8
dmong <rlaehdaud511@gmail.com>
2024-06-07 02:28:23 +0900

    first commit

---
+++ src/main/java/com/example/hello_springboot/HelloSpringbootApplication.java
@@ -0,0 +1,16 @@
+package com.example.hello_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringbootApplication.class, args);
+		System.out.println("HI");
+
+		
+	}
+
+}
